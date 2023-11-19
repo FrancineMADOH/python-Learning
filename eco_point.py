20a1a73eb8f4fb33c64a2ca324a6c4923e18cae0
FrancineMADOH <francinemadoh@gmail.com>
2023-11-19 15:08:27 +0100

    added the earn ecopoint function

--- eco_point.py
+++ eco_point.py
@@ -9,53 +9,49 @@ services = [
 
 # Input 
 
-number_of_ecopoint = int(input("Enter the number of ecopoint: "))
+number_of_ecopoint = input("Enter the number of ecopoint: ")
 services_to_reedem =  input("Select a service. 1=Voice, 2=Data, 3=Electricity: ")
+
+def earn_ecopoint(points,available_points):
+    total = int(points + available_points)
+    if total < 1000:
+        level = "Entry"
+    elif total > 1000:
+        level = "Bronze"
+    elif total > 2000:
+        level = "Gold"
+    else:
+        level = "Platinium"
+    
+    return level, total
 
 
 def redeem_ecopoint(point,service):
-
     try:
-        if point < entry :
+        if int(point) < entry :
            print("You are not yet eligible to redeem point yet")
            message = "You are not yet eligible to redeem point yet"
-        elif point >= entry:
+           return message
+        elif int(point)>=entry:
             for item in services:
                code =  item['code'] 
                base = item['points']
                name = item["service"]
 
                if service == code: 
-                   available_point = point - base
+                   available_point = int(point) - base
                    if available_point == 0:
+                       level = "Entry"
                        message = f"""You sucessfully redeem your ecopoint for {name} . You have {available_point} ecopoints  left."""
-                       #print(f"""You sucessfully redeem your ecopoint for {name} . You have {available_point} ecopoints  left.""")
-                       return message
+                       #print(f"""You sucessfully redeem your ecopoint for {name} . You have {available_point} ecopoints  left.""")                   
                    elif available_point > 0:
                         message  = f"""You sucessfully redeem your ecopoint for {name} . You have {available_point} ecopoints  left."""
-                        #print(f"""You sucessfully redeem your ecopoint for {name} . You have {available_point} ecopoints  left.""") 
+                       # print(f"""You sucessfully redeem your ecopoint for {name} . You have {available_point} ecopoints  left.""") 
                    else:
                     #print(f"""Sorry you are not eligible to this {name}. Points Needed:{item['points']}. Your points:{point}. Choose another service.""")
                     message = f"""Sorry you are not eligible to this { name }. Points Needed:{ item['points'] }. Your points:{point}. Choose another service."""
-                   
-            # get the level
-            if available_point < 1000:
-                level = "Entry"
-            elif available_point < 2000:
-                level = "Bronze"
-                return level
-            elif available_point < 3000:
-                level = "Gold"
-            elif available_point < 3000:
-                level = "Platinium"
-            else:
-                level = ""
-
-        print(available_point)
-        print(level)
-
-        return  available_point,level,message
-                                        
+            print(available_point)
+            return available_point  
     except:
         print("Error! Please enter valid data")
 
