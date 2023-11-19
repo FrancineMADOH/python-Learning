ffcc0bb34b861c7ab8a558df1d8e64791103d226
FrancineMADOH <francinemadoh@gmail.com>
2023-11-19 17:45:55 +0100

    added the price calculation

--- price_determination.py
+++ price_determination.py
@@ -1,15 +1,23 @@
 
-# constant
+# constants
 trash_size_list = [
     {"container":"Bucket","size":"10","code":"1","unit":"L"},
     {"container":"Trash Bag","size":"27","code":"2","unit":"L"},
     {"container":"Wheelbarrow","size":"80","code":"3","unit":"L"}
 ]
 
-# input 
+packages = [
+    {"name":"Pickup", "code": "1"},
+    {"name":"Cleaning", "code": "2"},
+    {"name":"pickup & Cleaning", "code": "3"}
+]
+
+# inputs 
 trash_size_input = input("Select your trash size. 1=(bucket-10L), 2=(trash bag-27L), 3=(wheelbarrow-80L): ")
 trash_quantity  = input("Enter your trash quantity: ")
-def get_trash_volume(code,quantity):
+trash_package = input("Choose package. 1=Pickup, 2=Cleanup, 3=Cleanup & Pickup: ")
+
+def get_trash_volume(code,quantity,package):
     
     try:
         for element in trash_size_list:
@@ -17,10 +25,17 @@ def get_trash_volume(code,quantity):
             trash_size = element['size']
             if trash_code == code:
                 trash_volume = (int(trash_size)/1000) * int(quantity)
-            else:
-                raise ValueError("Choice not available")
-        return trash_volume
+        for item in packages:
+            package_choose = item['code']
+            if package == package_choose:
+                price = trash_volume * 5000
+                if package == "1": 
+                    trash_price =  price
+                else:
+                    trash_price = 1.2 * price
+        print(trash_price)
+        return trash_price
     except:
         print("Error! Please enter valid data")
 
-get_trash_volume(trash_size_input,trash_quantity)
+get_trash_volume(trash_size_input,trash_quantity,trash_package)
