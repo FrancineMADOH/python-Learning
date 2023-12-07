832aee974c5a1cdedbb415f85a03f4fbfc4d1b5e
FrancineMADOH <francinemadoh@gmail.com>
2023-12-07 13:22:10 +0100

    configure the django

---
+++ performstarter/objectives/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+from . import models
+
+
+def landing_page(request):
+
+    return render(request, 'objectives/home.html', {})
+
+def view_objective(request):
+    return render(request, 'objectives/objective.html' ,{})
